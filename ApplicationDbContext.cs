0f0486b938cd26eee4a7808e81c3183ccee1728d
Jacob Sanchez <jacob_ico@outlook.com>
2023-05-02 17:09:45 -0600

    class Tarea added, class tarea set up in ApplicationDbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //applicationDbContext es arbitrario puedo usar cualquier nombre, el dbContext
+        //Podemos configurar las tablas de nuestras bases de datos y hacer consultas o cualquier tipo de operación.
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //Configuro nuestra clase Tarea como una entidad
+        public DbSet<Tarea> Tareas { get; set; }
+
+    }
+}
---
+++ Entidades/Tarea.cs
@@ -0,0 +1,13 @@
+﻿namespace TareasMVC.Entidades
+{
+    //Creación de una entidad
+    //Entidad es una representación de una tabla en C#
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Orden { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
