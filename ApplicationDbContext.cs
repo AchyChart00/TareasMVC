cbc60d7df54365080b737d74efd456cee441f27d
Jacob Sanchez <jacob_ico@outlook.com>
2023-05-04 10:27:16 -0600

    Migrations added

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -13,6 +13,14 @@ namespace TareasMVC
         }
         //Configuro nuestra clase Tarea como una entidad
         public DbSet<Tarea> Tareas { get; set; }
+        //Migraciones, es una representación en código de los cambios que va a ocurrir en la BD
+        //es un paso intermedio, ya que podemos ver que exactamente va a pasar con nuestra base de datos
+        //Para eso utilizamos el package manager console
+
+        //Para aplicar la migración debemos de abrir el Package Manager Console
+        //Ejecutamos el comando "Add-Migration Tareas"
+        //Después ejecutamos el comando Update-Database para aplicar los cambios a la base de datos
+
 
     }
 }
