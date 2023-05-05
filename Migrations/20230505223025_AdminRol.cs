bb4dc68a284a0d4405d1be76f18ed1a908750294
Jacob Sanchez <jacob_ico@outlook.com>
2023-05-05 16:41:59 -0600

    roles added

---
+++ Migrations/20230505223025_AdminRol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS(SELECT Id FROM AspNetRoles WHERE Id = '713416fd-1b99-4fbd-9803-70f190728e25')
+                                    BEGIN
+
+	                                    INSERT AspNetRoles(Id, [Name], [NormalizedName])
+	                                    VALUES('713416fd-1b99-4fbd-9803-70f190728e25', 'admin', 'ADMIN')
+
+                                    END
+                                    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM AspNetRoles WHERE Id = '713416fd-1b99-4fbd-9803-70f190728e25'");
+        }
+    }
+}
