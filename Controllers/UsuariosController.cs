ed309dae16c9fd2167d1680207e0b7a0e5974d47
Jacob Sanchez <jacob_ico@outlook.com>
2023-05-05 12:59:38 -0600

    actions for external login provider added

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using TareasMVC.Models;
 
 namespace TareasMVC.Controllers
@@ -65,8 +66,12 @@ namespace TareasMVC.Controllers
         }
 
         [AllowAnonymous]
-        public IActionResult Login()
+        public IActionResult Login(string mensaje = null)
         {
+            if (mensaje is not null)
+            {
+                ViewData["mensaje"] = mensaje;
+            }
             return View();
         }
 
@@ -103,6 +108,92 @@ namespace TareasMVC.Controllers
         {
             await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
             return RedirectToAction("Index", "Home");   
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        //Este método permite agregar un proveedor externo para poder logearse en nuestra aplicación
+        //ChallengeResult redirige al usuario a una fuente donde pueda logearse
+        public ChallengeResult LoginExterno(string proveedor, string urlRetorno = null)
+        {
+            var urlRedireccion = Url.Action("RegistrarUsuarioExterno", values:new { urlRetorno});
+            var propiedades = signInManager.ConfigureExternalAuthenticationProperties(proveedor, urlRedireccion);
+            return new ChallengeResult(proveedor, propiedades);
+        }
+
+        //Método que recibe la data del proveedor de identidad
+        [AllowAnonymous]
+        public async Task<IActionResult> RegistrarUsuarioExterno(
+                string urlRetorno =  null,
+                string remoteError = null
+            )
+        {
+            urlRetorno = urlRetorno ?? Url.Content("~/");
+
+            var mensaje = "";
+
+            if (remoteError is not null)
+            {
+                mensaje = $"Error del proveedor externo: {remoteError}";
+                return RedirectToAction("Login", routeValues: new { mensaje});   
+            }
+
+            var info = await signInManager.GetExternalLoginInfoAsync();
+
+            if (info is null) 
+            {
+                mensaje = "Error cargando la data del login externo";
+
+                return RedirectToAction("Login", routeValues: new { mensaje });
+            }
+
+            var resultadoLoginExterno = await signInManager.ExternalLoginSignInAsync(
+                info.LoginProvider,
+                info.ProviderKey,
+                isPersistent: true,
+                bypassTwoFactor: true
+                );
+
+            //Ya la cuenta existe
+            if (resultadoLoginExterno.Succeeded)
+            {
+                return LocalRedirect(urlRetorno);
+            }
+
+            string email = "";
+
+            if (info.Principal.HasClaim(c=>c.Type == ClaimTypes.Email))
+            {
+                email = info.Principal.FindFirstValue(ClaimTypes.Email);
+            }
+            else
+            {
+                mensaje = "Error leyendo el email del usuario del proveedor";
+                return RedirectToAction("login", routeValues: new { });
+            }
+
+            var usuario = new IdentityUser { Email = email, UserName = email };
+
+            var resultadoCrearUsuario = await userManager.CreateAsync(usuario);
+
+            if (!resultadoCrearUsuario.Succeeded)
+            {
+                mensaje = resultadoCrearUsuario.Errors.First().Description;
+                return RedirectToAction("login", routeValues: new { mensaje});
+            }
+
+            var resultadoAgregarLogin = await userManager.AddLoginAsync(usuario, info);
+
+            if (resultadoAgregarLogin.Succeeded)
+            {
+                await signInManager.SignInAsync(usuario, isPersistent:true, info.LoginProvider);
+                return RedirectToAction(urlRetorno);
+            }
+
+            mensaje = "Ha ocurrido un error agregando el login";
+            return RedirectToAction("login", routeValues: new { mensaje});
+
+
         }
     }
 }
