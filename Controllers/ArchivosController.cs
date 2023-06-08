3075a5e05edb3857cead6fc310d0f17ce4b8845c
Jacob Sanchez <jacob_ico@outlook.com>
2023-06-08 09:33:37 -0600

    saving attachments in DB

---
+++ Controllers/ArchivosController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+using TareasMVC.Servicios;
+using TareasMVC.Servicios.IServices;
+
+namespace TareasMVC.Controllers
+{
+    [Route("api/archivos")]
+    public class ArchivosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly IServicioUsuarios servicioUsuarios;
+        private readonly string contenedor = "archivosAdjuntos";
+
+        public ArchivosController(
+            ApplicationDbContext context,
+            IAlmacenadorArchivos almacenadorArchivos,
+            IServicioUsuarios servicioUsuarios
+            ) 
+        {
+            this.context = context;
+            this.almacenadorArchivos = almacenadorArchivos;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpPost("{tareaId:int}")]
+        public async Task<ActionResult<IEnumerable<ArchivoAdjunto>>>Post(
+            int tareaId,
+            //FromForm nos permite usar un formato especial a traves del cual podemos resivir data ya sea archivos como nombre, email, word, pdf, etc, así como colecciónes de archivos
+            [FromForm] IEnumerable<IFormFile> archivos
+            )
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var tarea = await context.Tareas
+                .FirstOrDefaultAsync(t => t.Id ==tareaId);
+
+            if ( tarea is null )
+            {
+                return NotFound();  
+            }
+
+            if (tarea.UsuarioCreacionId !=usuarioId)
+            {
+                return Forbid();
+            }
+
+            var existenArchivosAdjuntos = await context.ArchivosAdjuntos.AnyAsync(a=>a.TareaId == tareaId);
+
+            var ordenMayor = 0;
+
+            if (existenArchivosAdjuntos)
+            {
+                ordenMayor = await context.ArchivosAdjuntos
+                    .Where(a => a.TareaId == tareaId)
+                    .Select(a => a.Orden)
+                    .MaxAsync();
+            }
+
+            var resultados = await almacenadorArchivos.Almacenar(contenedor, archivos);
+
+            var archivosAdjuntos = resultados
+                .Select((resultado, indice)=> new ArchivoAdjunto
+                {
+                    TareaId = tareaId,
+                    FechaCreacion = DateTime.Now,
+                    Url = resultado.URL,
+                    Titulo = resultado.Titutlo,
+                    Orden = ordenMayor + indice + 1
+                }).ToList();
+
+            context.AddRange(archivosAdjuntos);
+
+            await context.SaveChangesAsync();   
+
+            return archivosAdjuntos.ToList();   
+        }
+    }
+}
---
+++ Servicios/IAlmacenadorArchivos.cs
@@ -0,0 +1,14 @@
+﻿using TareasMVC.Models;
+
+namespace TareasMVC.Servicios
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task Borrar(string ruta, string contenedor);
+        Task<AlmacenarArchivoResultado[]> Almacenar(
+            string contenedor, 
+            //IFormFile es el tipo de dato que utilizamos en ASP .NET CORE para representar un archivo
+            IEnumerable<IFormFile> archivos 
+            );
+    }
+}
